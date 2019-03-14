18bd57dc80aba28d8b9c617c0704d5120f751834
Ivan Kwong <ivan.kwong710@gmail.com>
2019-03-14 15:40:32 -0700

    Add search functionality and api call to the frontend

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,43 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const fetch = require("node-fetch");
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 5000;
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const fetch = require("node-fetch");
+// const bodyParser = require("body-parser");
+// const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
 
-// app.get("/", (req, res) => res.send("Hello World"));
+// app.get("/", (request, res) => {
+//   res.sendFile(path.join(__dirname, "./dist/index.html"));
+// });
 
-app.get("/", (request, res) => {
-  res.sendFile(path.join(__dirname, "./dist/index.html"));
-});
+// const formatTitle = (title) => {
+//   if(title.split(" ").length === 1) {
+//     return title.toLowerCase();
+//   } else {
+//     let result;
+//     result = title.toLowerCase().split(" ").join("-");
+//     return result;
+//   }
+// };
+
+// // route to get a single show with its main info
+// app.get('/shows/:title', (request, response) => {
+//   const title = formatTitle(request.params.title);
+
+//   fetch(`http://api.tvmaze.com/singlesearch/shows/?q=${title}`)
+//   .then((response) => {
+//     return response.text();
+//   }).then((body) => {
+//     let results = JSON.parse(body);
+//     console.log(results.id); //logs to server
+//     response.send(results); //sends to FE
+//   });
+// });
 
 
 
-app.listen(PORT, () => {
-  console.log(__dirname);
-  console.log(`listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(__dirname);
+//   console.log(`listening on ${PORT}`);
+// });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+const axios = require('axios');
 
 console.log('working');
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const getShow = () => {
+    const userInput = document.getElementById("showTitle").value;
+
+    axios
+    .get(`http://api.tvmaze.com/singlesearch/shows/?q=${userInput}`)
+    .then(response => {
+      let showId = response.data.id;
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+  
+  const showButton = document.getElementById("findShow");
+  showButton.addEventListener("click", getShow);
+  
+  
+});
+
 
