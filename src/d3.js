5e09ef509835be27d9d27a3a249f9869484d5fe0
Ivan Kwong <ivan.kwong710@gmail.com>
2019-03-16 20:04:39 -0700

    Add d3 code, and render sunburst with node data

--- src/d3.js
+++ src/d3.js
@@ -1,1 +1,96 @@
+import * as d3 from 'd3';
 
+
+
+const renderSunburst = nodeData => {
+  console.log(nodeData);
+  // Variables
+  let width = 500;
+  let height = 500;
+  let radius = Math.min(width, height) / 2;
+  // let color = d3.scaleOrdinal();
+
+  const dark = [
+    '#B08B12',
+    '#BA5F06',
+    '#8C3B00',
+    '#6D191B',
+    '#842854',
+    '#5F7186',
+    '#193556',
+    '#137B80',
+    '#144847',
+    '#254E00'
+  ];
+
+  const mid = [
+    '#E3BA22',
+    '#E58429',
+    '#BD2D28',
+    '#D15A86',
+    '#8E6C8A',
+    '#6B99A1',
+    '#42A5B3',
+    '#0F8C79',
+    '#6BBBA1',
+    '#5C8100'
+  ];
+
+  const light = [
+    '#F2DA57',
+    '#F6B656',
+    '#E25A42',
+    '#DCBDCF',
+    '#B396AD',
+    '#B0CBDB',
+    '#33B6D0',
+    '#7ABFCC',
+    '#C8D7A1',
+    '#A0B700'
+  ];
+
+  const palettes = [light, mid, dark];
+  const lightGreenFirstPalette = palettes
+    .map(d => d.reverse())
+    .reduce((a, b) => a.concat(b));
+
+  const color = d3.scaleOrdinal(lightGreenFirstPalette);
+
+  // Create primary <g> element
+  let g = d3.select('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+  // Data strucure
+  let partition = d3.partition()
+    .size([2 * Math.PI, radius]);
+
+  // Find data root
+  let root = d3.hierarchy(nodeData)
+    .sum(function (d) { return d.size });
+
+  // Size arcs
+  partition(root);
+  let arc = d3.arc()
+    .startAngle(function (d) { return d.x0 })
+    .endAngle(function (d) { return d.x1 })
+    .innerRadius(function (d) { return d.y0 })
+    .outerRadius(function (d) { return d.y1 });
+
+  // Put it all together
+  g.selectAll('path')
+    .data(root.descendants())
+    .enter().append('path')
+    .attr("display", function (d) { return d.depth ? null : "none"; })
+    .attr("d", arc)
+    .style('stroke', '#fff')
+    .style("fill", function (d) { return color((d.children ? d : d.parent).data.name); });
+
+
+ 
+  
+};
+
+export default renderSunburst;
--- src/index.js
+++ src/index.js
@@ -1,89 +1,169 @@
 const axios = require('axios');
-const HTMLParser = require('node-html-parser');
-import { parse } from "node-html-parser";
+// const HTMLParser = require('node-html-parser');
+// import { parse } from "node-html-parser";
+import renderSunburst from './d3';
 
 console.log('working');
 
 document.addEventListener('DOMContentLoaded', () => {
-  let showData = {
-    info: {},
-    seasons: {},
-    cast: {},
-    crew: {}
+
+  let nodeData = {
+    node: "",
+    children: [
+    {
+      node: "Info",
+      children: [{ "name": "Premiere Date", "value": ""}, {"name": "Rating", "value": ""}, {"name": "Runtime", "value": ""}, {"name": "Network", "value": ""},
+      {"name": "Summary", "value": ""}, {"name": "Genres", "children": []}]
+    },
+    {
+      node: "Seasons",
+      children: []
+    },
+    {
+      node: "Cast",
+      children: []
+    },
+    {
+      node: "Crew",
+      children: []
+    }
+    ]
   };
   
   const getShow = async () => {
     const userInput = document.getElementById("showTitle").value;
+    nodeData.node = userInput;
     
     const response = await axios.get(`http://api.tvmaze.com/singlesearch/shows/?q=${userInput}`); 
 
-    showData.id = response.data.id;
-    showData.info.premiereDate = response.data.premiered;
-    showData.info.genres = response.data.genres;
-    showData.info.rating = response.data.rating.average;
-    showData.info.runtime = response.data.runtime;
-    showData.info.summary = response.data.summary; //need to parse html
-    if (response.data.network) {
-      showData.info.network = response.data.network.name;
-    } else if (response.data.webChannel.name) {
-      showData.info.network = response.data.webChannel.name;
-    }
+    let showId = response.data.id;
 
-    const seasons = await axios.get(`http://api.tvmaze.com/shows/431/seasons`);   //get seasons
-    showData.seasons = seasons.data;
-   
-    for(let i = 0; i < showData.seasons.length; i++) {
-      let seasonId = showData.seasons[i].id;
-      
-      const episodes = await axios.get(`http://api.tvmaze.com/seasons/${seasonId}/episodes`);
-      showData.seasons[i] = episodes.data;
-    }
-    
-    const cast = await axios.get(`http://api.tvmaze.com/shows/${showData.id}/cast`);
-    showData.cast = cast.data;
-    // console.log(cast.data);
+    for (let a = 0; a < nodeData.children.length; a++) {
+      let child = nodeData.children[a];
+      if (child.node === "Info") {
+        for (let b = 0; b < child.children.length; b++) {
+          let innerChild = child.children[b];
+          if (innerChild.name === "Premiere Date") {
+            innerChild.value = response.data.premiered;
+            innerChild.size = 1;
+          } else if (innerChild.name === "Rating") {
+            innerChild.value = response.data.rating.average;
+            innerChild.size = 1;
+          } else if (innerChild.name === "Runtime") {
+            innerChild.value = response.data.runtime;
+            innerChild.size = 1;
+          } else if (innerChild.name === "Network") {
+            if (response.data.network) {
+              innerChild.value = response.data.network.name;
+            } else if (response.data.webChannel.name) {
+              innerChild.value = response.data.webChannel.name;
+            }
+            innerChild.size = 1;
+          } else if (innerChild.name === "Summary") {
+            innerChild.value = response.data.summary;
+            innerChild.size = 1;
+          } else if (innerChild.name === "Genres") {
+            for (let c = 0; c < response.data.genres.length; c++) {
+              let genre = response.data.genres[c];
+              let genreNode = {"name": genre, "size": 1};
+              innerChild.children.push(genreNode);
+            }
+          }
+        }
+      } else if (child.node === "Seasons") {
+        const seasons = await axios.get(`http://api.tvmaze.com/shows/${showId}/seasons`);   //get seasons
+        // child.size = seasons.data.length;
 
-    const crew = await axios.get(`http://api.tvmaze.com/shows/${showData.id}/crew`);
-    showData.crew = crew.data;
-    // console.log(crew.data);
-    
-    for(let j = 0; j < showData.cast.length; j++) {   //assign each cast member's person obj with one's other show urls
-      let member = showData.cast[j];
-      let personId = member.person.id;
-      
-      const otherShows = await axios.get(`http://api.tvmaze.com/people/${personId}/castcredits`); //get the person's other shows
-      showData.cast[j].person.showUrls = otherShows.data;
-      showData.cast[j].person.shows = [];
+        for (let i = 0; i < seasons.data.length; i++) {
+          let seasonId = seasons.data[i].id;
 
-      for (let k = 0; k < showData.cast[j].person.showUrls.length; k++) {
-        let showUrl = showData.cast[j].person.showUrls[k]._links.show.href;  //get show url
+          const episodes = await axios.get(`http://api.tvmaze.com/seasons/${seasonId}/episodes`);
+          child.children.push(episodes.data);
+        }
+     
+      } else if (child.node === "Cast") {
+        const cast = await axios.get(`http://api.tvmaze.com/shows/${showId}/cast`);
 
-        const show = await axios.get(showUrl);  //get the show
-        const showName = show.data.name;
-        showData.cast[j].person.shows.push(showName);
+        child.children = cast.data;
+      } else if (child.node === "Crew") {
+        const crew = await axios.get(`http://api.tvmaze.com/shows/${showId}/crew`);
+
+        child.children = crew.data;
       }
     }
-    
-    console.log(showData)
 
-    // const nodeDate = {
-    //   "root": `title`, 
-    //   "children": [{
-    //     "name1": "Basic Info",
-    //     "children": [{"Premiere Date": `${showData.info.premiereDate}`, "size": 1 }, {"Rating": `${showData.info.rating}`, "size": 1 }, {"Runtime": `${showData.info.runtime}`, "size": 1}, 
-    //                 {"Summary": `${showData.info.summary}`, "size": 1}, {"Genres": `${showData.info.genres}`, }]
-    //   }]
-    // }
+    for(let it = 0; it < nodeData.children.length; it++) {    //format seasons with children nodes 
+      let child = nodeData.children[it];
 
+      if(child.node === "Seasons") {
+        let seasonsChildren = [];
 
-  };
+        for(let e = 0; e < child.children.length; e++) {
+          let episodes = child.children[e];
+          let epChild = {"node": `Season ${e + 1}`, "children": []};
 
+          for(let l = 0; l < episodes.length; l++) {
+            let ep = episodes[l];
+            let epName = ep.name;
 
+            let epNode = {"name": epName, "size": 1};
+            epChild.children.push(epNode);
+          }
+
+          seasonsChildren.push(epChild);
+        }
+        child.children = seasonsChildren;
+        
+      } else if(child.node === "Cast") {
+        let castChildren = [];
+
+        for(let f = 0; f < child.children.length; f++) {
+          let char = child.children[f];
+          let personId = char.person.id;
+          let charChild = { "name": `${char.person.name}`, "children": [] };
+
+          const otherShows = await axios.get(`http://api.tvmaze.com/people/${personId}/castcredits`);
+          // console.log(otherShows.data);
+          for(let g = 0; g < otherShows.data.length; g++) {
+            let charCredit = otherShows.data[g];
+            let showUrl = charCredit._links.show.href;
+
+            const show = await axios.get(showUrl);
+            // console.log(show)
+            const showName = show.data.name;
+            let showNode = {"Show": showName, "size": 1};
+            charChild.children.push(showNode);
+          }
+          castChildren.push(charChild);
+        }
+        child.children = castChildren;
+      } else if(child.node === "Crew") {
+          let crewChildren = [];
+
+          for(let z = 0; z < child.children.length; z++) {
+            let crewMember = child.children[z];
+            let role = crewMember.type;
+            let name = crewMember.person.name;
+
+            let crewNode = {"name": name, "role": role, "size": 1};
+            crewChildren.push(crewNode);
+          }
+
+          child.children = crewChildren;
+        
+      }
+
+    }
+
+    // return nodeData;
+    // console.log(nodeData);
+    renderSunburst(nodeData);
+  }
+
+   
+  
   const showButton = document.getElementById("findShow");
   showButton.addEventListener("click", getShow);
-
-
-
   
 });
 
